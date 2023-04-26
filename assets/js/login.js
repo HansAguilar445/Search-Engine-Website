aa08262553d0192fd62a2f27b26810ed87f4d5c5
Hans Aguilar <hans.christian.aguilar@gmail.com>
2023-04-26 18:35:01 -0500

    Initial commit

---
+++ assets/js/login.js
@@ -0,0 +1,143 @@
+'use strict'
+
+//Variables: Modal
+const modal = document.querySelector('#modal');
+const blocker = document.querySelector('#blocker');
+const exit = document.querySelector('#close');
+const error = document.querySelector('#error');
+const $login = document.forms['signin'];
+const $username = document.forms['signin']['username'];
+const $password = document.forms['signin']['password'];
+const $send = document.forms['signin']['send'];
+
+//Variables: Header
+const signin = document.querySelector('#signin');
+const translate = document.querySelector('#translate');
+const videos = document.querySelector('#videos');
+const account = document.querySelector('#account');
+let loggedIn = false;
+let isEnglish = true;
+
+//Username and password - referencing Transformers G1
+const user = 'spikewitwicky';
+const pass = 'morethanmeetstheeye';
+
+//Variables: Search
+const searchbar = document.querySelector('#search-bar');
+const bing = document.getElementById('bing');
+const google = document.getElementById('google');
+
+
+//Modal: Activating and deactivating
+blocker.addEventListener('click', function() {
+	modal.classList.remove('visible');
+});
+exit.addEventListener('click', function() {
+	modal.classList.remove('visible');
+});
+
+signin.addEventListener('click', function() {
+	modal.classList.add('visible');
+});
+
+//Modal: Login
+$send.addEventListener('click', function() {
+	let valid = true;
+	let username = $username.value.trim();
+	let password = $password.value.trim();
+	let issues = 'The following issues must be corrected:\n';
+	let issueCount = 0;
+	//Checking if username and/or password are empty
+	if (username === '') {
+		valid = false;
+		issues += 'Empty username field\n';
+		issueCount++;
+		$username.classList.add('invalid');
+	} else {
+		if (username != user) {
+			valid = false;
+			issues += 'Incorrect username\n';
+			issueCount++;
+			$username.classList.add('invalid');
+		} else {
+			if ($username.classList.contains('invalid'))
+				$username.classList.remove('invalid');
+		}
+	}
+	
+	if (password === '') {
+		valid = false;
+		issues += 'Empty password field';
+		issueCount++;
+		$password.classList.add('invalid');
+	} else {
+		if (password != pass) {
+			valid = false;
+			issues += 'Incorrect password';
+			issueCount++;
+			$password.classList.add('invalid');
+		} else {
+			if ($password.classList.contains('invalid'))
+				$password.classList.remove('invalid');
+		}
+	}
+
+	if(!valid && issueCount === 2) {
+		error.innerText = 'Please fill out both fields correctly.'
+	} else if (!valid && issueCount < 2) {
+		error.innerText = issues;
+	} else {
+		bing.disabled = false;
+		google.disabled = false;
+		$username.value = '';
+		$password.value = '';
+		signin.innerHTML = 'Spike <i class="far fa-user" id="user"></i>';
+		loggedIn = true;
+		modal.classList.remove('visible');
+	}
+	
+});
+
+//Search
+
+google.addEventListener('click', function() {
+	let search = searchbar.value.trim();
+	let searchQuery = 'https://google.com/search?q=' + search;
+	if (search != '') {
+		searchbar.value = '';
+		window.location.assign(searchQuery);
+	}
+});
+
+bing.addEventListener('click', function() {
+	let search = searchbar.value.trim();
+	let searchQuery = 'https://bing.com/search?q=' + search;
+	if (search != '') {
+		searchbar.value = '';
+		window.location.assign(searchQuery);
+	}
+});
+
+//Translating the page for authenticity
+translate.addEventListener('click', function() {
+	isEnglish = !isEnglish;
+	if (isEnglish) {
+		if (!loggedIn)
+			signin.innerHTML = 'Sign In <i class="far fa-user" id="user"></i>';
+		account.innerText = 'Create Account';
+		videos.innerText = 'Videos';
+		translate.innerText = 'Français';
+		google.value = 'Search Google';
+		bing.value = 'Search Bing';	
+		searchbar.placeholder = 'Search';
+	} else {
+		if (!loggedIn)
+			signin.innerHTML = 'Connexion <i class="far fa-user" id="user"></i>';
+		account.innerText = 'Créer un compte';
+		videos.innerText = 'Vidéos';
+		translate.innerText = 'English';
+		google.value = 'Recherche Google';
+		bing.value = 'Recherche Bing';
+		searchbar.placeholder = 'Recherche';	
+	}
+});
---
+++ assets/js/signup.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const $form = document.forms['registration'];
+const $fullName = document.querySelector('#full-name');
+const $age = document.querySelector('#age');
+const $email = document.querySelector('#email');
+const $username = document.querySelector('#username');
+const $password = document.querySelector('#password');
+const $send = document.querySelector('#send');
+const error = document.querySelector('#error');
+
+const emailRegex = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/;
+$send.addEventListener('click', function() {
+	let fullName = $fullName.value.trim();
+	let age = $age.value.trim();
+	let email = $email.value.trim();
+	let username = $username.value.trim();
+	let password = $password.value.trim();
+	let issueCount = 0;
+
+	let issues = 'The following issues must be corrected:\n';
+	let valid = true;
+
+	if (fullName === '') {
+		issues += 'Your full name is required\n';
+		valid = false;
+		issueCount++;
+		$fullName.classList.add('invalid');
+	} else {
+		if ($fullName.classList.contains('invalid'))
+			$fullName.classList.remove('invalid');
+	}
+
+	if (age === '') {
+		issues += 'Age field is empty\n';
+		valid = false;
+		issueCount++;
+		$age.classList.add('invalid');
+	} else {
+		let ageInt = parseInt(age);
+		if (isNaN(age) || ageInt < 13) {
+			issues += 'Invalid age\n';
+			valid = false;
+			issueCount++;
+			$age.classList.add('invalid');
+		} else {
+			if ($age.classList.contains('invalid'))
+				$age.classList.remove('invalid');
+		}
+	}
+
+	if (email === '') {
+		issues += 'Email field is empty\n';
+		valid = false;
+		issueCount++;
+		$email.classList.add('invalid');
+	} else {
+		if (!emailRegex.test(email)) {
+			issues += 'Email is invalid\n';
+			valid = false;
+			issueCount++;
+			$email.classList.add('invalid');
+		} else {
+			if ($email.classList.contains('invalid'))
+				$email.classList.remove('invalid');
+		}
+	}
+	
+	if (username === '') {
+		issues += 'Username field is empty\n';
+		valid = false;
+		issueCount++;
+		$username.classList.add('invalid');
+	} else {
+		if ($username.classList.contains('invalid'))
+			$username.classList.remove('invalid');
+	}
+
+	if (password.length < 10) {
+		issues += 'Password must be at least 10 characters';
+		valid = false;
+		issueCount++;
+		$password.classList.add('invalid');
+	} else {
+		if ($password.classList.contains('invalid'))
+			$password.classList.remove('invalid');
+	}
+
+	if (!valid && issueCount > 4) {
+		error.innerText = 'Please fill out all the fields.'
+	} else if (!valid && issueCount <= 4) {
+		error.innerText = issues;
+	} else {
+		fullName = '';
+		age = '';
+		email = '';
+		username = '';
+		password = '';
+		error.innerText = 'Success!';
+	}
+});
